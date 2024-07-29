bd5dc0d945db5d9a6b9f615e002d5a14df044263
kuvaldastalina <m.kopytenko@yandex.ru>
2024-07-29 19:54:37 +0300

    Update Homework6.py

--- Homework6.py
+++ Homework6.py
@@ -1,13 +1,21 @@
 mu_dict = {'Roman': 2006, 'Dima': 1893}
-print(mu_dict['Dima'])
 mu_dict['John'] = 2004
-mu_dict.update({'Olga': 1344,
-                'Misha': 2001})
-del mu_dict['Olga']
+mu_dict.update({'Olga': 1344, 'Misha': 2001})
 print(mu_dict)
+
+print(mu_dict['Dima'])
+print(mu_dict.get('Egor'))
+
+
+a = mu_dict.pop('Olga')
+print(mu_dict)
+print(a)
+
 print(mu_dict.items())
 
 my_set = {1, 1, 2, 3, 3, 3, 'Bread'}
-print(my_set.add(75))
+print(my_set.add(6))
+print(my_set.add(7))
 print(my_set.remove(2))
 print(my_set)
+)
